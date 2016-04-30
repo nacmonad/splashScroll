e52c31a46f608eac7c745c6a246b54a29d3b6c4b
nacmonad <macnomad84@gmail.com>
2016-04-30 16:57:41 -0400

    ui-router, although not neccessary.  added rows to splashpage view.

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,50 @@
-var myApp = angular.module('splashPage',['ui.bootstrap']);
+var myApp = angular.module('splashPage',['ui.bootstrap','ui.router']);
+
+myApp.config(['$urlRouterProvider','$stateProvider', function($urlRouterProvider,$stateProvider){
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+			.state('main', {
+				url: '/main', 
+				templateUrl: '/public/templates/main.html', 
+				controller: 'MainCtrl'
+			})
+			.state('one', {
+				url: '/one', 
+				templateUrl: '/public/templates/one.html' 
+			
+			}).state('two', {
+				url: '/two', 
+				templateUrl: '/public/templates/two.html' 
+			
+			})
+			.state('three', {
+				url: '/three', 
+				templateUrl: '/public/templates/three.html'  
+			
+			});
+	}]);
 
 myApp.controller("MainCtrl", function ($scope) {
 	console.log("MainCtrl loaded");
 	$scope.msg = "hello world";
 	$scope.bang = function () {
 		alert($scope.msg);
-	}
-})
+		}
+	})
+	.controller('NavCtrl', function ($scope) {
+		console.log("NavCtrl loaded");
+
+		$scope.sections = ['Main','One','Two','Three'];
+		$scope.searchKey ='';
+
+		$scope.search = function (key) {
+			key ? alert("search key " + key) : alert("please enter a search term");
+		}
+		$scope.activeTab = function (index) {
+			console.log("tab" + index);
+		}
+	})
+	.controller('FtrCtrl', function ($scope) {
+		console.log("FtrCtrl loaded");
+		
+	});
